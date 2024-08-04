17fe2a466645df21e194e87865930d4623084a5b
Sarthak Joshi <sarthaksj02@gmail.com>
2024-08-04 10:46:41 +0530

    change cors url

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(
 	cors({
 		credentials: true,
-		origin: ["http://localhost:5173", "https://motomaps-frontend.vercel.app"],
+		origin: ["http://localhost:5173", "https://motomaps.vercel.app"],
 	}),
 )
 
